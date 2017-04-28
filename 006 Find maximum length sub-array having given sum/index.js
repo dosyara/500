e46a66b80ae926090a19a33d76a8c9dc3324756c
Sergey Maximov <sergey.maximov@booking.com>
2017-04-28 14:24:07 +0200

    006 linear solution

--- 006 Find maximum length sub-array having given sum/index.js
+++ 006 Find maximum length sub-array having given sum/index.js
@@ -31,4 +31,32 @@ function findArrayForSum(arr, sum) {
     return result;
 }
 
+function findArrayForSum2(arr, sum) {
+    let result = [];
+    let map = {};
+    let len = 0;
+    let ending_index = -1;
+    let crntSum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        crntSum += arr[i];
+
+        console.log(crntSum, map);
+        if (!(crntSum in map)) {
+            map[crntSum] = i;
+        }
+
+        let name = (crntSum - sum) + '';
+        if (name in map && len < i - map[name]) {
+            console.log('?', name);
+            len =  i - map[name];
+            ending_index = i;
+        }
+
+    }
+
+    return {ending_index, len, result: arr.slice(ending_index - len + 1, ending_index + 1)};
+}
+
 console.dir(findArrayForSum([5, 6, -5, 5, 3, 5, 3, -2, 0], 8));
+console.dir(findArrayForSum2([5, 6, -5, 5, 3, 5, 3, -2, 0], 8));
