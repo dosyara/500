a328aba1fffc89d30b3bdf225027193dd82b1c3c
Sergey Maximov <sergey.maximov@booking.com>
2017-06-06 12:18:00 +0200

    010 Array Merge two arrays by satisfying given constraints squash and merge solution

--- 010 Array Merge two arrays by satisfying given constraints/index.js
+++ 010 Array Merge two arrays by satisfying given constraints/index.js
@@ -67,8 +67,37 @@ function shiftLeftToVacant(arr, pos) {
     }
 }
 
+function squashAndMerge(arr1, arr2) {
+    squashArray(arr1);
+    mergeWithOrder(arr1, arr2);
+}
+
+function squashArray(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr1[i] === 0) {
+            for (let j = i + 1; j < arr.length; j++) {
+                if (arr[j] !== 0) {
+                    [arr[i], arr[j]] = [arr[j], arr[i]];
+                    break;
+                }
+            }
+        }
+    }
+}
+
+function mergeWithOrder(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i] === 0 || (arr2[j] !== 0 && arr1[i] > arr2[j])) {
+                [arr1[i], arr2[j]] = [arr2[j], arr1[i]];
+            }
+        }
+    }
+}
+
 let arr1 = [0, 2, 0, 3, 0, 5, 6, 0, 0];
 let arr2 = [1, 8, 9, 10, 15];
-mergeVacantNaive(arr1, arr2);
+// mergeVacantNaive(arr1, arr2);
+squashAndMerge(arr1, arr2);
 
 console.log(arr1);
